789fd0ac536dcfa7f16085e95c63f9d18e2ab8f7
Cristian Duque <ardilacristian54@gmail.com>
2025-11-29 22:01:38 -0500

    feat(utils): agregar finción sumar con validaciones y pruebas rápidas

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ function mostrarSaludo(nombre) {
     return resultado;
 }
 
-// Export para futuras pruebas o uso modular (CommonJS por ahora)
-if (typeof module !== "undefined" && module.exports) {
-    module.exports = { saludar, mostrarSaludo };
+function sumar(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "Error: ambos valores deben ser números.";
+    }
+    return a + b;
 }
 
-// Ejecución si se corre directo
+if (typeof module !== "undefined" && module.exports) {
+    module.exports = { saludar, mostrarSaludo, sumar };
+}
+
 if (require.main === module) {
-    mostrarSaludo("Cristian - Commit 1");
+    mostrarSaludo("Cristian - Commit 2");
+    console.log("Suma 10 + 20 =", sumar(10, 20));
+    console.log("Prueba suma inválida:", sumar("x", 5));
 }
